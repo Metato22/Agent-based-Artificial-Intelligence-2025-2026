f5f7f2919e1f6d1c8ac8793a2a1b4c2ac92209ec
Matteo Di Maria <matteodimaria03@gmail.com>
2026-04-23 02:04:34 +0200

    Refactor robot navigation code and fix import path

--- examples/problem/main_robotnav_DiMaria.py
+++ examples/problem/main_robotnav_DiMaria.py
@@ -2,7 +2,7 @@
 
 from path_search.search import Search
 from path_search.strategies import GreedStrategy
-from robot_navigation import RobotNavigationProblem
+from problems.robotNavigation.DiMaria import RobotNavigationProblem
 
 # Definiamo una griglia 5x5
 # Mettiamo un muro di osticali in mezzo
